1a17f68076727c92b48ac7bd7982b033d9eec454
Jessica <jessica@kpd.com.tw>
2016-12-08 18:14:57 +0800

    新增資源站頁面

---
+++ application/controllers/Source.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Source extends MY_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+   var $data = array();
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index(){
+      $this->finaldata["page_name"] = "資源站管理";
+      $this->twig->display("source_index",$this->finaldata);
+    }
+}
